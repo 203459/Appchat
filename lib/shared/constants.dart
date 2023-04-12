b7134c7cc3f0fbf3aeb9c233434a35f3e2c01322
203459 <203459@ids.upchiapas.edu.mx>
2023-04-11 22:49:22 -0600

    the project is added chatApp

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:541638971855:web:b80cb1213d4a55f64182ba";
+  static String apiKey = "AIzaSyB1ZnBv-Qj0YUUUq3ov0FBx_e0AB2DNUYQ";
+  static String messagingSenderId = "541638971855";
+  static String projectId = "chatt-105e8";
+  final primaryColor = const Color.fromARGB(255, 100, 125, 238);
+}
